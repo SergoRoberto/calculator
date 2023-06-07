247de92abc4fa5add1fcd57f30bb0f58396dc531
Sergo Roberto <rudakov.private.bsf@gmail.com>
2023-06-07 20:46:44 +0300

    calculate

---
+++ app/calculate/calculate.go
@@ -0,0 +1,17 @@
+package calculate
+
+func Add(x int, y int) int {
+	return x + y
+}
+
+func Subtract(x int, y int) int {
+	return x - y
+}
+
+func Multiply(x int, y int) int {
+	return x * y
+}
+
+func Divide(x int, y int) int {
+	return x / y
+}
--- app/main.go
+++ app/main.go
@@ -1,7 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"app/calculate"
+	"fmt"
+	"strconv"
+	"strings"
+)
 
 func main() {
-	fmt.Println("Hello world")
+
+	calculator("2 + 2, 3 - 3, 4 * 3, 12 / 3")
+
+}
+
+func calculator(str string) string {
+
+	exps := strings.Split(str, ", ")
+	res := make(chan string)
+	for _, exp := range exps {
+		sym := strings.Split(exp, " ")
+		x, err := strconv.Atoi(sym[0])
+		if err != nil {
+			return "error"
+		}
+		y, err := strconv.Atoi(sym[2])
+		if err != nil {
+			return "error"
+		}
+
+		switch sym[1] {
+		case "+":
+			go calculate.Add(x, y)
+		case "-":
+			go calculate.Subtract(x, y)
+		case "*":
+			go calculate.Multiply(x, y)
+		case "/":
+			go calculate.Divide(x, y)
+		}
+		fmt.Println(sym[1])
+	}
+
+	return <-res
 }
